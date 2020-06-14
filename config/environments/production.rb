6c414997e4d8ab471c3bfefed28f6469e0a75c1c
Francis John Banlasan <francisjohn.banlasan@gmail.com>
2020-06-15 02:30:02 +0800

    Finalize Redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.middleware.use ChatActionCable  
-  config.web_socket_server_url = "wss://chat-app-122415.herokuapp.com/"
+  config.web_socket_server_url = "wss://chat-app-122415.herokuapp.com/cable"
 
   
 end
