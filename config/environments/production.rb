4208301473bca4e8c1a18e08d33d7ad64a24cd5a
Francis John Banlasan <francisjohn.banlasan@gmail.com>
2020-06-15 02:26:11 +0800

    Finalize Redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_cable.url = "wss://chat-app-122415.herokuapp.com/cable"
+  config.middleware.use ChatActionCable  
+  config.web_socket_server_url = "wss://chat-app-122415.herokuapp.com/"
 
   
 end
